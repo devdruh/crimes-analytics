0e0adf920e67f5cd136878743bb29fc03f0760b4
devdruh <dev.drewragadio@gmail.com>
2024-04-04 18:56:22 -0400

    remove comment

--- src/pages/error/NotFound.jsx
+++ src/pages/error/NotFound.jsx
@@ -2,7 +2,6 @@ import { useRouteError } from 'react-router-dom'
 
 const NotFound = () => {
   const error = useRouteError();
-  // console.log(error," not found");
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       { error.statusText || error.message }
