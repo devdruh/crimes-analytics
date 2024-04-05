136bd20b99de4211dc601c04410103058d2bdba9
devdruh <dev.drewragadio@gmail.com>
2024-04-04 20:09:20 -0400

    responsive home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,11 +3,11 @@ import LeftSidebar from "../../components/sidebar/LeftSidebar"
 
 const Home = () => {
   return (
-    <div className="flex flex-row w-full gap-2">
-      <div className="basis-1/6">
+    <div className="flex w-full gap-2">
+      <div className="basis-1/6 max-sm:basis-full">
         <LeftSidebar />
       </div>
-      <div className="basis-10/12">
+      <div className="basis-10/12 max-sm:basis-full">
         <MapContainer />
       </div>
     </div>
