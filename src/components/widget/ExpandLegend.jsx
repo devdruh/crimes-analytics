aec544349b55b82425401ca675a61a2bbbf403ab
devdruh <dev.drewragadio@gmail.com>
2024-04-25 16:39:03 -0400

    legend widget tooltip

--- src/components/widget/ExpandLegend.jsx
+++ src/components/widget/ExpandLegend.jsx
@@ -5,6 +5,7 @@ import view from "../map/View";
 const expandLegend = new Expand({
     // expandIcon: "layers",  // see https://developers.arcgis.com/calcite-design-system/icons/
     // expandTooltip: "Expand LayerList", // optional, defaults to "Expand" for English locale
+    expandTooltip: 'View Legend',
     view: view,
     content: legend
 });
