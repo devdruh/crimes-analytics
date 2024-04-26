e9bce3e5d27ee8b5ce46960a4b7b0c1fd1a9eb31
devdruh <dev.drewragadio@gmail.com>
2024-04-25 20:26:24 -0400

    change nav title

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
     return (
         <div className="navbar bg-base-100 border-b border-neutral-content">
             <div className="flex-1">
-                <a className="btn btn-link text-xl no-underline hover:no-underline text-opacity-90">Crime Analytics</a>
+                <a className="btn btn-link text-xl no-underline hover:no-underline text-opacity-90">Toronto Crimes Analytics</a>
             </div>
             <div className="navbar-end">
                 <div className="mr-2">
