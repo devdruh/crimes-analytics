ca20f8ff85c603385a095ba30de7d48ee48e23ea
devdruh <dev.drewragadio@gmail.com>
2024-05-02 15:58:41 -0400

    update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,17 +1,20 @@
 const Footer = () => {
     return (
-        <div className="h-14 flex justify-center items-center">
+        <div className="h-14 w-full flex flex-row justify-center items-center">
             {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3338467565655011" crossOrigin="anonymous"></script> */}
             {/* <!-- Ad unit - responsive template --> */}
-            <ins className="adsbygoogle block"
+            <div className="grow">
+                <ins className="adsbygoogle"
                 // style="display:block"
-                data-ad-client="ca-pub-3338467565655011"
-                data-ad-slot="9491494907"
-                data-ad-format="auto"
-                data-full-width-responsive="true"></ins>
-            {/* <script>
+                    style={{ display: 'block' }}
+                    data-ad-client="ca-pub-3338467565655011"
+                    data-ad-slot="9491494907"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+                {/* <script>
                 (adsbygoogle = window.adsbygoogle || []).push({ });
             </script> */}
+            </div>
         </div>
     )
 }
