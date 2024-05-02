17cb169aab2d09f0fd435ed578f3ec9a5537772c
devdruh <dev.drewragadio@gmail.com>
2024-05-02 15:32:43 -0400

    adsense unit, footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+    return (
+        <div className="h-14 flex justify-center items-center">
+            {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3338467565655011" crossOrigin="anonymous"></script> */}
+            {/* <!-- Ad unit - responsive template --> */}
+            <ins className="adsbygoogle block"
+                // style="display:block"
+                data-ad-client="ca-pub-3338467565655011"
+                data-ad-slot="9491494907"
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>
+            {/* <script>
+                (adsbygoogle = window.adsbygoogle || []).push({ });
+            </script> */}
+        </div>
+    )
+}
+
+export default Footer
