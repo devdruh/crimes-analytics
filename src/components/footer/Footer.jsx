5cb8ad86f16a3a3a21560d1a1f4c7290a27eddc0
devdruh <dev.drewragadio@gmail.com>
2024-05-02 22:59:38 -0400

    footer enhancement

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,27 +1,21 @@
-import { useEffect } from "react"
-
 const Footer = () => {
 
-    useEffect(() => {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-    }, []);
-
     return (
-        <footer className="h-14 w-full flex flex-row justify-center items-center">
-            {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3338467565655011" crossOrigin="anonymous"></script> */}
+        <footer className="flex flex-row justify-center items-center">
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3338467565655011" crossOrigin="anonymous"></script>
             {/* <!-- Ad unit - responsive template --> */}
-            <div className="flex min-w-96">
+            <div className="flex h-14 min-w-96">
                 {/* Footer */}
                 <ins className="adsbygoogle block"
-                // style="display:block"
                     style={{ display: 'block', minWidth: '100%' }}
                     data-ad-client="ca-pub-3338467565655011"
                     data-ad-slot="9491494907"
                     data-ad-format="auto"
-                    data-full-width-responsive="true"></ins>
-                {/* <script>
-                (adsbygoogle = window.adsbygoogle || []).push({ });
-            </script> */}
+                    data-full-width-responsive="true">
+                </ins>
+                <script>
+                    {(Window.adsbygoogle = window.adsbygoogle || []).push({})}
+                </script>
             </div>
         </footer>
     )
