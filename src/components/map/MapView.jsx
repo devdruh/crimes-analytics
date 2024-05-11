54f8d0b1b2fe24e0e06e1d2c66e52bdf807ddb34
devdruh <dev.drewragadio@gmail.com>
2024-05-11 00:03:13 -0400

    change mapview size

--- src/components/map/MapView.jsx
+++ src/components/map/MapView.jsx
@@ -1,13 +1,15 @@
 import { useParams } from "react-router-dom";
+import createActiveTab from "../../zustand/createActiveTab";
 
 // eslint-disable-next-line react/prop-types
 const MapView = ({ appRef, mapRef }) => {
 
     const { month } = useParams();
+    const { activeTab } = createActiveTab();
 
     return (
         <div id="app-map-container" className="app-map-container" ref={appRef}>
-            <div className={`${month !== undefined ? 'map-container-slider' : 'map-container ease-out duration-300'}`}>
+            <div className={`${month !== undefined ? 'map-container-slider' : `${activeTab === 3 ? 'map-container-frequency ease-out' : 'map-container ease-out'}`}`}>
                 <div id="viewDiv" ref={mapRef}></div>
             </div>
         </div>
@@ -15,37 +17,3 @@ const MapView = ({ appRef, mapRef }) => {
 }
 
 export default MapView
-
-// return month !== undefined ? <div id="app-map-container" className="app-map-container" ref={appRef}>
-//     <div className='map-container-slider'>
-//         <div id="viewDiv" ref={mapRef}></div>
-//     </div>
-// </div> : <div id="app-map-container" className="app-map-container" ref={appRef}>
-//     <div className='map-container ease-out duration-300'>
-//         <div id="viewDiv" ref={mapRef}></div>
-//     </div>
-// </div>
-
-// < div id = "app-map-container" className = "app-map-container" ref = { appRef } >
-//     <div className="map-container">
-//         <div id="viewDiv" ref={mapRef}>
-
-//             {
-//                 // loading && (
-//                 //     <div className="flex absolute justify-end right-2 translate-y-1/2">
-//                 //         <span id="map-loading" className="esri-widget loading loading-ring loading-lg"></span>
-//                 //     </div>
-//                 // )
-//             }
-//         </div>
-
-//         {
-//             // isMapLoading ? <span id="map-loading" className="esri-widget map-loading loading loading-ring loading-lg"></span> : ''
-//             // <div className="flex justify-end right-2 top-1/2">
-//             //     <span id="map-loading" className="esri-widget loading loading-ring loading-lg"></span>
-//             // </div>
-//         }
-
-
-//     </div>
-// </div >
