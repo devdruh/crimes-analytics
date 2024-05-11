a44dbae857084b1d1aa3e476623b886ce6842f90
devdruh <dev.drewragadio@gmail.com>
2024-05-10 23:44:12 -0400

    store selected tab

--- src/components/tab/TabContainer.jsx
+++ src/components/tab/TabContainer.jsx
@@ -9,12 +9,14 @@ const TabContainer = ({ items }) => {
 
     useEffect(() => {
 
-        if (activeTab === 3) {
-            expandChart.visible = false;
-        } else {
-            expandChart.visible = true;
+        activeTab === 3 ? expandChart.visible = false : expandChart.visible = true;
+
+        const localActiveTab = localStorage.getItem('activeTab');
+        if (localActiveTab !== null) {
+            setActiveTab(parseInt(localActiveTab));
         }
-    }, [activeTab]);
+
+    }, [setActiveTab, activeTab]);
 
     return (
         <div role="tablist" className="tabs tabs-lifted tabs-lg mt-2">
@@ -23,7 +25,7 @@ const TabContainer = ({ items }) => {
                 // eslint-disable-next-line react/prop-types
                 items.map((tab) => (
                     <React.Fragment key={tab.id}>
-                        <a role="tab" className={`tab ${tab.id === activeTab ? 'tab-active' : ''}`} onClick={() => setActiveTab(tab.id)}>{tab.title}</a>
+                        <a role="tab" className={`tab ${tab.id === activeTab ? 'tab-active' : ''}`} onClick={() => { setActiveTab(tab.id); localStorage.setItem('activeTab', tab.id) }}>{tab.title}</a>
                         <div role="tabpanel" className="tab-content bg-base-100 border-base-300 dark:border-neutral rounded-box rounded-b-none">
                             {tab.content}
                         </div>
