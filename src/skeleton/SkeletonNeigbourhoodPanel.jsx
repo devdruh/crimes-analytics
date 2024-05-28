fa1e82e81eb640274dced6f0e51da432e41ada6e
devdruh <dev.drewragadio@gmail.com>
2024-05-28 18:57:18 -0400

    neighbourhood panel skeleton

---
+++ src/skeleton/SkeletonNeigbourhoodPanel.jsx
@@ -0,0 +1,42 @@
+// eslint-disable-next-line react/prop-types
+const SkeletonNeigbourhoodPanel = ({ data }) => {
+
+    return (
+        <>
+            {
+                // eslint-disable-next-line react/prop-types
+                data && data.map((item, index) => (
+                    <div className="stat" key={index}>
+                        <div className="stat-figure text-secondary">
+                            <div className="avatar">
+                                <div className="skeleton w-16 h-16 max-sm:w-12 max-sm:h-12 rounded-full"></div>
+                            </div>
+                        </div>
+                        <div className="stat-title"><div className="skeleton h-3 w-1/3 mt-2 max-md:h-4 max-md:mt-1"></div></div>
+                        <div className="stat-value"><div className="skeleton h-8 w-1/2 mt-2 max-md:h-5"></div></div>
+                        <div className="stat-desc"><div className="skeleton h-2 w-5/6 mt-2 mb-1"></div></div>
+                    </div>
+                ))
+            }
+
+            {
+                // eslint-disable-next-line react/prop-types
+                data.length === 0 && [...Array(9)].map((_, index) => (
+
+                    <div className="stat" key={index}>
+                        <div className="stat-figure text-secondary">
+                            <div className="avatar">
+                                <div className="skeleton w-16 h-16 max-sm:w-12 max-sm:h-12 rounded-full"></div>
+                            </div>
+                        </div>
+                        <div className="stat-title"><div className="skeleton h-3 w-1/3 mt-2 max-md:h-4 max-md:mt-1"></div></div>
+                        <div className="stat-value"><div className="skeleton h-8 w-1/2 mt-2 max-md:h-5"></div></div>
+                        <div className="stat-desc"><div className="skeleton h-2 w-5/6 mt-2 mb-1"></div></div>
+                    </div>
+                ))
+            }
+        </>
+    )
+}
+
+export default SkeletonNeigbourhoodPanel
